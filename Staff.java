2ae8ccb028184f6e19e0f9bc6810ce346a26c019
Gunavelk <93586392+Gunavelk@users.noreply.github.com>
2021-11-08 21:04:18 +0530

    Add files via upload

---
+++ Staff.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class Staffdetails{
+	int experience=1;
+	void details(String name,int exp,int salary,String sub) {
+		System.out.println("Name:"+name +"\nExp:" +exp+"\nSalary:"+salary+ "\nSubject:"+sub);
+	}
+	void details(String name,int salary, String sub) {
+		System.out.println("Name:"+name+"\nExp:"+experience+"\nSalary:"+salary+"\nSubject:"+sub);
+	}
+}
+
+public class Staff{
+	public static void main(String args[]) {
+		Staffdetails s=new Staffdetails();
+		s.details("raj",4,12000,"Maths");
+		s.details("Mohan",18000,"English");
+	}
+}
